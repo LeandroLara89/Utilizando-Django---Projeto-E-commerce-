0c666dee1fd97e97f7c39fba53a880efa0c35c3e
Leandro <leandrodsilvalara@yahoo.com.br>
2020-03-02 17:50:53 -0300

    Slug automático e cadastro de produtos

--- produto/models.py
+++ produto/models.py
@@ -3,6 +3,8 @@ from django.db import models
 import os
 from PIL import Image
 from django.conf import settings
+from django.db import models
+from django.utils.text import slugify
 
 
 class Produto(models.Model):
@@ -11,17 +13,25 @@ class Produto(models.Model):
     descricao_longa = models.TextField()
     imagem = models.ImageField(
         upload_to='produto_imagens/%Y/%m/', blank=True, null=True)
-    slug = models.SlugField(unique=True)
-    preco_marketing = models.FloatField()
-    preco_marketing_promocional = models.FloatField(default=0)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    preco_marketing = models.FloatField(verbose_name='Preço')
+    preco_marketing_promocional = models.FloatField(default=0, verbose_name='Preço Promo.')
     tipo = models.CharField(
         default='V',
         max_length=1,
         choices=(
-            ('V', 'Variação'),
+            ('V', 'Variável'),
             ('S', 'Simples'),
         )
     )
+
+    def get_preco_formatado(self):
+        return f'R$ {self.preco_marketing:.2f}'.replace('.', '.')
+    get_preco_formatado.short_description = 'Preço'
+
+    def get_preco_promocional_formatado(self):
+        return f'R$ {self.preco_marketing_promocional:.2f}'.replace('.', '.')
+    get_preco_promocional_formatado.short_description = 'Preço Promo.'
 
     @staticmethod
     def resize_image(img, new_width=800):
@@ -51,6 +61,12 @@ class Produto(models.Model):
         """
 
     def save(self, *args, **Kwargs):
+        if not self.slug:
+            slug = f'{slugify(self.nome)}'
+            self.slug = slug
+
+
+
         super().save(*args, **Kwargs)
 
         max_image_size = 800
