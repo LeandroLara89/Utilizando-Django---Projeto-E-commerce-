fcc5618fef54a7e403ce1f23c8c4f2e90c826c15
Leandro <leandrodsilvalara@yahoo.com.br>
2020-03-03 17:29:27 -0300

    Listando os produtos templates

--- pedido/urls.py
+++ pedido/urls.py
@@ -5,6 +5,6 @@ app_name = 'pepido'
 
 urlpatterns = [
     path('', views.Pagar.as_view(), name='pagar'),
-    path('fecharpedido', views.FecharPedido.as_view(), name='fecharpedido'),
-    path('', views.Detalhe.as_view(), name='detalhe'),
+    path('fecharpedido/', views.FecharPedido.as_view(), name='fecharpedido'),
+    path('detalhe/', views.Detalhe.as_view(), name='detalhe'),
 ]
